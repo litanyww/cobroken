74edf8d2ee8e9a36c634eca4710b03097fae9171
William Waghorn <william.waghorn@sophos.com>
2025-06-03 15:21:36 -0700

    Add a fix

--- cobroken.cpp
+++ cobroken.cpp
@@ -13,30 +13,66 @@
 #include <set>
 #include <variant>
 
-#define DBGOUT(...) do { std::ostringstream ost; ost << std::this_thread::get_id() << " - " << __VA_ARGS__ << "\n"; std::cerr << ost.str(); } while (0)
+#define DBGOUT(...)                                                        \
+    do                                                                     \
+    {                                                                      \
+        std::ostringstream ost;                                            \
+        ost << std::this_thread::get_id() << " - " << __VA_ARGS__ << "\n"; \
+        std::cerr << ost.str();                                            \
+    } while (0)
+
+constexpr bool withFix = true;
 
 std::stop_source stop{};
 
 class HistoryAdd
 {
-    void* from{};
-    void* to{};
+    void *from{};
+    void *to{};
     std::thread::id t{std::this_thread::get_id()};
+
 public:
     HistoryAdd(std::coroutine_handle<> f, std::coroutine_handle<> t) : from{f.address()}, to{t.address()} {}
-    void Show(std::ostream& ost) const
+    void Show(std::ostream &ost) const
     {
         ost << t << " transfer from " << from << " to " << to;
     }
 };
 
+class HistoryTChain
+{
+    void *co{};
+    void *dest{};
+    std::thread::id t{std::this_thread::get_id()};
+
+public:
+    HistoryTChain(std::coroutine_handle<> c, std::coroutine_handle<> d) : co{c.address()}, dest{d.address()} {}
+    void Show(std::ostream &ost) const
+    {
+        ost << t << " thread local chain from " << co << " to " << dest;
+    }
+};
+class HistoryTWoke
+{
+    void *co{};
+    std::thread::id t{std::this_thread::get_id()};
+
+public:
+    HistoryTWoke(std::coroutine_handle<> c) : co{c.address()} {}
+    void Show(std::ostream &ost) const
+    {
+        ost << t << " thread local woke " << co;
+    }
+};
+
 class HistoryResume
 {
-    void* co{};
+    void *co{};
     std::thread::id t{std::this_thread::get_id()};
+
 public:
     HistoryResume(std::coroutine_handle<> c) : co{c.address()} {}
-    void Show(std::ostream& ost) const
+    void Show(std::ostream &ost) const
     {
         ost << t << " await_resume " << co;
     }
@@ -44,12 +80,13 @@ public:
 
 class HistorySuspend
 {
-    void* co{};
+    void *co{};
     bool how{};
     std::thread::id t{std::this_thread::get_id()};
+
 public:
     HistorySuspend(std::coroutine_handle<> c, bool h) : co{c.address()}, how{h} {}
-    void Show(std::ostream& ost) const
+    void Show(std::ostream &ost) const
     {
         ost << t << (how ? " suspending " : " resuming ") << co;
     }
@@ -57,11 +94,12 @@ public:
 
 class HistoryBusted
 {
-    void* co{};
+    void *co{};
     std::thread::id t{std::this_thread::get_id()};
+
 public:
     HistoryBusted(std::coroutine_handle<> c) : co{c.address()} {}
-    void Show(std::ostream& ost) const
+    void Show(std::ostream &ost) const
     {
         ost << t << " busted " << co;
     }
@@ -69,15 +107,16 @@ public:
 
 class History
 {
-    using Type = std::variant<HistoryAdd, HistoryResume, HistorySuspend, HistoryBusted>;
+    using Type = std::variant<HistoryAdd, HistoryResume, HistorySuspend, HistoryBusted, HistoryTChain, HistoryTWoke>;
     inline static constexpr size_t maximum = 2000UL;
     std::vector<Type> vec_{};
     unsigned int offset_{};
     mutable std::mutex mutex_{};
+
 public:
     History() { vec_.reserve(maximum); }
     template <typename Entry>
-    void Record(Entry&& entry)
+    void Record(Entry &&entry)
     {
         std::scoped_lock guard{mutex_};
         if (vec_.size() < maximum)
@@ -87,7 +126,7 @@ public:
             return;
         }
         offset_ = (offset_ % maximum);
-        Type& item = vec_[offset_];
+        Type &item = vec_[offset_];
         ++offset_;
         item = std::forward<Entry>(entry);
     }
@@ -101,9 +140,9 @@ public:
         DBGOUT(" --- history " << vec.size() << " ---");
 
         std::ostringstream ost;
-        for (size_t i = 0U ; i < vec.size() ; ++i)
+        for (size_t i = 0U; i < vec.size(); ++i)
         {
-            std::visit([&ost](auto&& arg) mutable
+            std::visit([&ost](auto &&arg) mutable
                        { arg.Show(ost); }, vec[--o % maximum]);
             ost << "\n";
         }
@@ -113,7 +152,8 @@ public:
 
 History history{};
 
-namespace {
+namespace
+{
     std::atomic<unsigned long> transferCount{};
 }
 
@@ -125,6 +165,7 @@ public:
         std::atomic_bool suspended_{};
         std::thread::id suspendedBy_{};
         std::thread::id wakeOn_{};
+        bool watch_{};
         Task get_return_object() { return Task{std::coroutine_handle<promise_type>::from_promise(*this)}; }
         std::suspend_never initial_suspend() noexcept { return {}; }
         std::suspend_never final_suspend() noexcept { return {}; }
@@ -139,12 +180,11 @@ public:
 };
 using CoroutineHandle = std::coroutine_handle<Task::promise_type>;
 
-
-
 class Handles
 {
-    std::mutex mutex_{};
+    mutable std::mutex mutex_{};
     std::deque<CoroutineHandle> handles_{};
+
 public:
     void Add(CoroutineHandle h)
     {
@@ -152,10 +192,17 @@ public:
         handles_.push_back(h);
     }
 
+    size_t size() const
+    {
+        std::scoped_lock guard{mutex_};
+        return handles_.size();
+    }
+
     CoroutineHandle Get()
     {
         std::scoped_lock guard{mutex_};
-        if (handles_.empty()) {
+        if (handles_.empty())
+        {
             return {};
         }
         auto h = handles_.back();
@@ -164,14 +211,109 @@ public:
     }
 };
 
+class Fix
+{
+    class Co
+    {
+    public:
+        struct promise_type
+        {
+            Co get_return_object() { return Co{std::coroutine_handle<promise_type>::from_promise(*this)}; }
+            std::suspend_always initial_suspend() noexcept { return {}; }
+            std::suspend_never final_suspend() noexcept { return {}; }
+            void unhandled_exception() {}
+            void return_void() {}
+        };
+
+        Co(std::coroutine_handle<promise_type> handle) : handle_{handle} {}
+        ~Co() { handle_.destroy(); }
+        std::coroutine_handle<> GetHandle() { return handle_; }
+
+    private:
+        std::coroutine_handle<promise_type> handle_{};
+    };
+
+    class Awaiter
+    {
+        std::coroutine_handle<> m_destination{};
+        std::coroutine_handle<> m_me{};
+        std::thread::id m_thread{std::this_thread::get_id()};
+
+    public:
+        Awaiter(std::coroutine_handle<> destination) : m_destination{destination} {}
+        bool await_ready() const noexcept { return false; }
+        std::coroutine_handle<> await_suspend(std::coroutine_handle<> me) const noexcept
+        {
+            history.Record(HistoryTChain(me, m_destination));
+            return m_destination;
+        }
+        void await_resume() const noexcept
+        {
+            HistoryTWoke(Fix::Instance().m_destination);
+        }
+    };
+
+private:
+    Fix() = default;
+
+public:
+    Co Run()
+    {
+        std::thread::id correctThread{std::this_thread::get_id()};
+        for (;;)
+        {
+            co_await Awaiter{m_destination};
+            if (std::this_thread::get_id() != correctThread)
+            {
+                DBGOUT(" XXX wrong thread, dropping " << m_destination.address());
+                CoroutineHandle h{CoroutineHandle::from_address(m_destination.address())};
+                h.promise().watch_ = true;
+                m_lost.Add(h);
+                co_await std::suspend_always();
+                DBGOUT(" XXX back after suspend_always");
+            }
+        }
+    }
+
+    static Fix &Instance()
+    {
+        static thread_local Fix fix{};
+        return fix;
+    }
+
+    std::coroutine_handle<> Chain(std::coroutine_handle<> next)
+    {
+        if constexpr (withFix)
+        {
+            m_destination = next;
+            return m_task.GetHandle();
+        }
+        else
+        {
+            return next;
+        }
+    }
+
+    CoroutineHandle GetLost()
+    {
+        return m_lost.Get();
+    }
+
+private:
+    inline static thread_local std::coroutine_handle<> m_destination{};
+    Co m_task{Run()};
+    Handles m_lost{};
+};
+
 bool success{true};
 
 class Transfer
 {
-    Handles& handles_;
+    Handles &handles_;
     CoroutineHandle destination_{};
     CoroutineHandle me_{};
     CoroutineHandle mine_{};
+
 public:
     Transfer(Handles &handles, CoroutineHandle destination, CoroutineHandle mine) : handles_{handles}, destination_{destination}, mine_{mine}
     {
@@ -180,7 +322,8 @@ public:
     std::coroutine_handle<> await_suspend(CoroutineHandle me)
     {
         me_ = me;
-        if (me != mine_) {
+        if (me != mine_)
+        {
             DBGOUT(" XXX suspend is suspending the wrong coroutine " << me.address() << " when we thought we were in " << mine_.address());
         }
         me.promise().suspendedBy_ = std::this_thread::get_id();
@@ -189,10 +332,12 @@ public:
         std::coroutine_handle<> destination = destination_;
         handles_.Add(me);
         ++transferCount;
-        return destination;
+        return Fix::Instance().Chain(destination);
     }
-    bool await_resume() {
-        if (!me_) {
+    bool await_resume()
+    {
+        if (!me_)
+        {
             DBGOUT("awaiter did not suspend!");
         }
         history.Record(HistoryResume{me_});
@@ -203,23 +348,26 @@ public:
         }
         history.Record(HistorySuspend(me_, false));
         return true;
-
     }
 };
-
 
 class GetMine
 {
     CoroutineHandle me_{};
+
 public:
     bool await_ready() { return false; }
-    bool await_suspend(CoroutineHandle me) { me_ = me; return false; }
+    bool await_suspend(CoroutineHandle me)
+    {
+        me_ = me;
+        return false;
+    }
     CoroutineHandle await_resume() { return me_; }
 };
 
 thread_local CoroutineHandle expected{};
 
-Task Op(std::stop_token token, Handles& handles)
+Task Op(std::stop_token token, Handles &handles)
 {
     CoroutineHandle mine = co_await GetMine{};
     handles.Add(mine);
@@ -228,8 +376,14 @@ Task Op(std::stop_token token, Handles& handles)
     DBGOUT("first " << mine.address());
     while (!token.stop_requested())
     {
-        if (CoroutineHandle next = handles.Get())
+        if (CoroutineHandle next = [&]
+            { if (CoroutineHandle h = Fix::Instance().GetLost()) { return h; } return handles.Get(); }())
         {
+            if (next.promise().watch_)
+            {
+                DBGOUT("Executing watched coroutine " << next.address());
+                next.promise().watch_ = false;
+            }
             expected = next;
             history.Record(HistoryAdd{mine, next});
             // DBGOUT(" > " << mine.address() << " transferring to " << next.address());
@@ -240,7 +394,8 @@ Task Op(std::stop_token token, Handles& handles)
                 DBGOUT("unsuspended coroutine started, me=" << mine.address() << ", didn't start " << expected.address());
                 success = false;
             }
-            if (mine.promise().wakeOn_ != std::this_thread::get_id()) {
+            if (mine.promise().wakeOn_ != std::this_thread::get_id())
+            {
                 DBGOUT("unexpectedly did not wake " << mine.address() << " on the expected thread " << mine.promise().wakeOn_);
                 success = false;
             }
@@ -253,7 +408,8 @@ Task Op(std::stop_token token, Handles& handles)
                 stop.request_stop();
             }
         }
-        else {
+        else
+        {
             DBGOUT("no handles");
             history.Show();
             co_await std::suspend_always();
@@ -262,7 +418,7 @@ Task Op(std::stop_token token, Handles& handles)
     }
 }
 
-void Run(std::stop_token token, Handles* handles)
+void Run(std::stop_token token, Handles *handles)
 {
     DBGOUT("starting thread");
     static_cast<void>(token);
@@ -281,18 +437,19 @@ bool test()
     std::vector<std::jthread> threads_{};
     std::vector<Task> tasks_{};
 
-    for (unsigned int i = 0 ; i < 7 ; ++i)
+    for (unsigned int i = 0; i < 7; ++i)
     {
         Task op = Op(stop.get_token(), handles);
         tasks_.push_back(std::move(op));
     }
 
-    for (unsigned int i = 0 ; i < 3 ; ++i)
+    for (unsigned int i = 0; i < 3; ++i)
     {
         threads_.push_back(std::jthread{&Run, &handles});
     }
     std::this_thread::sleep_for(std::chrono::seconds{1U});
-    std::cerr << "stopping" << std::endl;;
+    std::cerr << "stopping" << std::endl;
+    ;
     stop.request_stop();
     std::cout << "success=" << std::boolalpha << success << ", " << transferCount.load() << " transfers" << std::endl;
     return success;
--- minimal.cpp
+++ minimal.cpp
@@ -10,11 +10,18 @@
 #include <sstream>
 #include <vector>
 
-#define DBGOUT(...) do { std::ostringstream ost; ost << std::this_thread::get_id() << " - " << __VA_ARGS__ << "\n"; std::cerr << ost.str(); } while (0)
+#define DBGOUT(...)                                                        \
+    do                                                                     \
+    {                                                                      \
+        std::ostringstream ost;                                            \
+        ost << std::this_thread::get_id() << " - " << __VA_ARGS__ << "\n"; \
+        std::cerr << ost.str();                                            \
+    } while (0)
 
 std::stop_source stop{};
 
-namespace {
+namespace
+{
     std::atomic<unsigned long> transferCount{};
 }
 
@@ -37,10 +44,76 @@ public:
 };
 using CoroutineHandle = std::coroutine_handle<Task::promise_type>;
 
+class Fix
+{
+    class Co
+    {
+    public:
+        struct promise_type
+        {
+            Co get_return_object() { return Co{std::coroutine_handle<promise_type>::from_promise(*this)}; }
+            std::suspend_always initial_suspend() noexcept { return {}; }
+            std::suspend_never final_suspend() noexcept { return {}; }
+            void unhandled_exception() {}
+            void return_void() {}
+        };
+
+        Co(std::coroutine_handle<promise_type> handle) : handle_{handle} {}
+        ~Co() { handle_.destroy(); }
+        std::coroutine_handle<> GetHandle() { return handle_; }
+
+    private:
+        std::coroutine_handle<promise_type> handle_{};
+    };
+
+    class Awaiter
+    {
+        std::coroutine_handle<> m_destination{};
+
+    public:
+        Awaiter(std::coroutine_handle<> destination) : m_destination{destination} {}
+        bool await_ready() const noexcept { return false; }
+        std::coroutine_handle<> await_suspend(std::coroutine_handle<>) const noexcept
+        {
+            return m_destination;
+        }
+        void await_resume() const noexcept {}
+    };
+
+private:
+    Fix() = default;
+
+public:
+    Co Run()
+    {
+        for (;;)
+        {
+            co_await Awaiter{m_destination};
+        }
+    }
+
+    static Fix &Instance()
+    {
+        static thread_local Fix fix{};
+        return fix;
+    }
+
+    std::coroutine_handle<> Chain(std::coroutine_handle<> next)
+    {
+        m_destination = next;
+        return m_task.GetHandle();
+    }
+
+private:
+    inline static thread_local std::coroutine_handle<> m_destination{};
+    Co m_task{Run()};
+};
+
 class Handles
 {
     std::mutex mutex_{};
     std::deque<CoroutineHandle> handles_{};
+
 public:
     void Add(CoroutineHandle h)
     {
@@ -51,7 +124,8 @@ public:
     CoroutineHandle Get()
     {
         std::scoped_lock guard{mutex_};
-        if (handles_.empty()) {
+        if (handles_.empty())
+        {
             return {};
         }
         auto h = handles_.back();
@@ -64,8 +138,9 @@ bool success{true};
 
 class Transfer
 {
-    Handles& handles_;
+    Handles &handles_;
     CoroutineHandle destination_{};
+
 public:
     Transfer(Handles &handles, CoroutineHandle destination) : handles_{handles}, destination_{destination}
     {
@@ -76,7 +151,7 @@ public:
         std::coroutine_handle<> destination = destination_;
         handles_.Add(me);
         ++transferCount;
-        return destination;
+        return Fix::Instance().Chain(destination);
     }
     void await_resume()
     {
@@ -86,15 +161,20 @@ public:
 class GetMine
 {
     CoroutineHandle me_{};
+
 public:
     bool await_ready() { return false; }
-    bool await_suspend(CoroutineHandle me) { me_ = me; return false; }
+    bool await_suspend(CoroutineHandle me)
+    {
+        me_ = me;
+        return false;
+    }
     CoroutineHandle await_resume() { return me_; }
 };
 
 thread_local CoroutineHandle expected{};
 
-Task Op(std::stop_token token, Handles& handles)
+Task Op(std::stop_token token, Handles &handles)
 {
     CoroutineHandle mine = co_await GetMine{};
     handles.Add(mine);
@@ -113,14 +193,15 @@ Task Op(std::stop_token token, Handles& handles)
                 stop.request_stop();
             }
         }
-        else {
+        else
+        {
             DBGOUT("no handles");
             co_await std::suspend_always();
         }
     }
 }
 
-void Run(std::stop_token token, Handles* handles)
+void Run(std::stop_token token, Handles *handles)
 {
     static_cast<void>(token);
     if (CoroutineHandle h = handles->Get())
@@ -136,18 +217,19 @@ bool test()
     std::vector<std::jthread> threads_{};
     std::vector<Task> tasks_{};
 
-    for (unsigned int i = 0 ; i < 7 ; ++i)
+    for (unsigned int i = 0; i < 7; ++i)
     {
         Task op = Op(stop.get_token(), handles);
         tasks_.push_back(std::move(op));
     }
 
-    for (unsigned int i = 0 ; i < 3 ; ++i)
+    for (unsigned int i = 0; i < 3; ++i)
     {
         threads_.push_back(std::jthread{&Run, &handles});
     }
     std::this_thread::sleep_for(std::chrono::seconds{1U});
-    std::cerr << "stopping" << std::endl;;
+    std::cerr << "stopping" << std::endl;
+    ;
     stop.request_stop();
     std::cout << "success=" << std::boolalpha << success << ", " << transferCount.load() << " transfers" << std::endl;
     return success;
